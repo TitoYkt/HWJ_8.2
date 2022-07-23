9898c095b81f689625427a401c02500c464293e0
Tito <Zakh.tito@mail.ru>
2022-07-23 19:11:31 +0700

    commit

--- src/test/java/ru/netology/statistic/StaticServiceTest.java
+++ src/test/java/ru/netology/statistic/StaticServiceTest.java
@@ -5,26 +5,4 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StaticServiceTest {
-
-    @Test
-    void findMax() {
-        StaticService service = new StaticService();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
-        long actual = service.findMax(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void findMaxIfTheMaxValueIsTheLast() {
-        StaticService service = new StaticService();
-        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
-        long actual = service.findMax(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
 }
